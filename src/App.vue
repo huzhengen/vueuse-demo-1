04318ace82753c4e0523f84f866e5d58570d4310
huzhengen <huzhengen@163.com>
2023-09-09 18:07:56 +0800

    Add vueuse, add Mouse

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Mouse from './components/Mouse.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+  <Mouse />
   <HelloWorld msg="Vite + Vue" />
 </template>
 
---
+++ src/components/Mouse.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useMouse, usePreferredDark, useLocalStorage } from '@vueuse/core'
+
+const { x, y } = useMouse()
+
+const isDark = usePreferredDark()
+console.log(isDark.value)
+
+const store = useLocalStorage('my-storage', {
+  name: 'Apple',
+  color: 'red',
+})
+console.log(store.value)
+</script>
+
+<template>
+  <h3>Mouse: {{ x }} x {{ y }}</h3>
+</template>
+
+<style scoped></style>
