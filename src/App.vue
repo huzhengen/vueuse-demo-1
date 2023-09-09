978efc1a09eaa68d829fc8bf18e25b49161a0916
huzhengen <huzhengen@163.com>
2023-09-09 18:14:25 +0800

    Add Clipboard

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import Mouse from './components/Mouse.vue'
+import Clipboard from './components/Clipboard.vue'
 </script>
 
 <template>
+  <Clipboard />
   <Mouse />
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Clipboard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useClipboard } from '@vueuse/core'
+
+const source = ref('Copy this text')
+
+const { text, copy, copied, isSupported } = useClipboard({ source })
+</script>
+
+<template>
+  <div v-if="isSupported">
+    <button @click="copy(source)">
+      <!-- by default, `copied` will be reset in 1.5s -->
+      <span v-if="!copied">{{ source }}</span>
+      <span v-else style="color: green">Copied!</span>
+    </button>
+    <p>
+      Current copied: <code>{{ text || 'none' }}</code>
+    </p>
+  </div>
+  <p v-else>Your browser does not support Clipboard API</p>
+  <hr />
+</template>
+
+<style scoped></style>
